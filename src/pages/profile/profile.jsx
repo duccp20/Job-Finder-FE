6f87ce69885870c504cb0498c56ebc490100db43
phuoc <duccp20@gmail.com>
2023-12-08 23:19:21 +0700

    add outlet for profile, change route

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 
 import { Toggle } from "../../components/Toggle";
-import pen from "/public/svg/pen.svg";
-import HeaderHome from "../../components/HeaderHome";
 
-const Profile = (props) => {
+import HeaderHome from "../../components/HeaderHome";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const isAuthenticated = useSelector((state) => state.account.isAuthenticated);
+  const user = useSelector((state) => state.account.user);
+  const navigate = useNavigate();
   return (
     <div>
       <HeaderHome />
-      <div className="flex h-screen w-full px-[100px] mt-[60px] gap-[24px] items-start">
+      <div className="flex h-auto w-full px-[100px] gap-[24px] items-start my-[60px]">
         <div className="border border-[#FE5656] rounded-[10px] w-[40%] h-auto py-[50px] px-[45px] shadow-banner">
           <img
             className="aspect-square mx-auto rounded-full object-cover bg-center bg-no-repeat w-[200px] h-[200px]"
             src="https://images.unsplash.com/photo-1701084412727-1f3e01088a5f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
           />
-          {/* <h2>{props.name}</h2> */}
+
           <h3 className="text-red-500 text-center text-2xl not-italic font-extrabold mt-[35px] mb-[45px]">
-            Tran Dang
+            {isAuthenticated && user && user.firstName + " " + user.lastName}
           </h3>
           <hr className="mb-[35px] border-[1px]" />
           <div className="flex items-center">
@@ -31,102 +36,7 @@ const Profile = (props) => {
             nhiều cơ hội việc làm tốt từ IT Jobs.
           </p>
         </div>
-        <div className="flex flex-col w-[60%] h-auto gap-5">
-          <div className="h-[45%] rounded-[10px] shadow-banner">
-            <div className=" rounded-tl-[10px] rounded-tr-[10px] rounded-bl-[0px] rounded-br-[0px] bg-[#FE5656] w-full h-[55px] flex justify-between py-[14px] px-[44px] shadow-banner">
-              <span className="text-xl not-italic font-bold text-white ">
-                Thông tin cá nhân
-              </span>
-              <a href="#" className="">
-                <img src={pen} alt="" />
-              </a>
-            </div>
-            <div className="table py-[30px] px-[40px]">
-              <table className="min-w-full">
-                <tbody className="text-left">
-                  <tr className="">
-                    <th className="py-[8px] px-[4px]">Họ và tên lót</th>
-                    <td className="py-[8px] px-[4px]">Nguyen</td>
-                    <th className="py-[8px] pl-[50px]">Tên</th>
-                    <td className="py-[8px] px-[4px]">Hoa</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Email </th>
-                    <td className="py-[8px] px-[4px]">
-                      nguyenhoa123@gmail.com
-                    </td>
-                    <th className="py-[8px] pl-[50px]">Ngày sinh </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Số điện thoại </th>
-                    <td className="py-[8px] px-[4px]">0982151558</td>
-                    <th className="py-[8px] pl-[50px]">Giới tính </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Tỉnh/ Thành phố </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                    <th className="py-[8px] pl-[50px]">Quận/ Huyện </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Địa chỉ </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Trường học </th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="h-[55%] mt-[px] shadow-banner rounded-[10px]">
-            <div className="rounded-tl-[10px] rounded-tr-[10px] rounded-bl-[0px] rounded-br-[0px] bg-[#FE5656] w-full h-[55px] flex justify-between py-[14px] px-[44px] shadow-banner">
-              <span className="text-xl not-italic font-bold text-white ">
-                Thông tin công việc muốn ứng tuyển
-              </span>
-              <a href="#" className="">
-                <img src={pen} alt="" />
-              </a>
-            </div>
-            <div className="table py-[30px] px-[40px]">
-              <table className="min-w-full">
-                <tbody className="text-left">
-                  <tr className="">
-                    <th className="py-[8px] px-[4px]">Công việc mong muốn</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Vị trí làm việc</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Chuyên ngành</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Hình thức làm việc</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Địa điểm làm việc</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">CV đính kèm</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                  <tr>
-                    <th className="py-[8px] px-[4px]">Thư xin việc</th>
-                    <td className="py-[8px] px-[4px]">(chưa có dữ liệu)</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <Outlet></Outlet>
       </div>
     </div>
   );
