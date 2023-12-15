94a417e3a426939c11a6f4cf1637b8549282c132
Tran4654 <tuyettranchidung@gmail.com>
2023-12-15 14:57:25 +0700

    fix conflict

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -25,9 +25,9 @@ const Popup = (props) => {
     };
   }, []);
   return (
-    <div className="w-screen h-full overflow-hidden bg-black bg-opacity-30 flex items-center justify-center shadow-custom absolute z-[100000] top-0">
-      <div className="w-[35%] h-auto px-[10px] py-[25px] bg-white rounded-[46px] shadow-blur backdrop-blur-[6.800000190734863px]">
-        <h1 className="text-[30px] font-[800] uppercase text-center mt-[30px] leading-normal	">
+    <div className="fixed left-0 top-0 z-[999999] flex h-full w-screen items-center justify-center overflow-hidden bg-black bg-opacity-30 shadow-custom">
+      <div className="h-auto w-[35%] rounded-[46px] bg-white px-[10px] py-[25px] shadow-blur backdrop-blur-[6.800000190734863px]">
+        <h1 className="mt-[30px] text-center text-[30px] font-[800] uppercase leading-normal	">
           {props.text}
         </h1>
         <div className="my-[30px] flex justify-center">
